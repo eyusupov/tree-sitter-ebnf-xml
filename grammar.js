de84c66ed8ec1462d9ca21236b579ee350e1105b
Eldar Yusupov <eyusupov@gmail.com>
2024-05-15 11:47:03 +0300

    Allow newlines only in alternations

--- grammar.js
+++ grammar.js
@@ -1,9 +1,12 @@
 module.exports = grammar({
   name: 'ebnf',
-  extras: $ => [/\n/, /\s/, $.comments],
-  conflicts: $ => [[ $.char_match, $.range ]],
+  extras: $ => [/\s/, $.comments],
+  conflicts: $ => [
+    [ $.char_match, $.range ],
+    [ $.body, $._expression_opt_nl],
+  ],
   rules: {
-    grammar: $ => seq(repeat(seq($.rule, '\n')), optional($.rule)),
+    grammar: $ => seq(repeat(seq($.rule, repeat1('\n'))), optional($.rule)),
     rule: $ => seq(optional($.label), $.name, '::=', $.body),
     label: $ => /\[[0-9A-Za-z]+\]/,
     body: $ => $.expression,
@@ -25,8 +28,7 @@ module.exports = grammar({
       $.zero_or_more
     ),
     rule_ref: $ => $.symbol,
-    hex_char: $ => $.hex_integer,
-    hex_integer: $ => /#x[0-9a-fA-F]+/,
+    hex_char: $ => $.hex_integer, hex_integer: $ => /#x[0-9a-fA-F]+/,
     char_match: $ => seq('[', optional($.neg_marker), repeat(choice($._char_atom, $.range)), ']'),
     neg_marker: $ => '^',
     match_character: $ => /[^\]-]/,
@@ -41,7 +43,8 @@ module.exports = grammar({
     double_quoted_string: $ => token(choice(/"([^"]|(\\\\"))*"/, /"\\""/)),
     optional: $ => prec(3, seq($.expression, '?')),
     sequence: $ => prec.left(2, seq($.expression, $.expression)),
-    alternation: $ => prec.left(1, seq($.expression, '|', $.expression)),
+    _expression_opt_nl: $ => seq($.expression, optional('\n')),
+    alternation: $ => prec.left(1, seq($._expression_opt_nl, '|', $.expression)),
     difference: $ => prec(2, prec.left(seq($.expression, '-', $.expression))),
     one_or_more: $ => prec(3, seq($.expression, '+')),
     zero_or_more: $ => prec(3, seq($.expression, '*')),
