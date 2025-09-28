b418dce0d752b398225f6688646d0e0acff95a20
Eldar Yusupov <eyusupov@gmail.com>
2025-09-28 20:33:19 +0300

    Update highlight queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,2 +1,18 @@
-(double_quoted_string) @string.grammar
-(single_quoted_string) @string.grammar
+(double_quoted_string) @string
+(single_quoted_string) @string
+(label) @comment
+(symbol) @keyword
+(char_match) @character.special
+(hex_char) @string
+[
+  "::="
+  "?"
+  "+"
+  "*"
+  "|"
+  (neg_marker)
+] @operator
+[
+ "("
+ ")"
+] @punctuation.bracket
